0d50f66f9370133b1bae2b176d4bde18bcd3b6a2
adityaa369 <adityaamruthaluri369@gmail.com>
2026-08-24 19:00:25 +0530

    chore(security): remove temporary test scripts containing environment variables and credentials

--- fixEngine.js
+++
@@ -1,4 +0,0 @@
-﻿const fs = require('fs');
-let code = fs.readFileSync('sockets/auctionEngine.js', 'utf8');
-code = code.replace(/function initAuctionEngine\(server\) \{\s*let ioInstance;/g, "let ioInstance;\nfunction initAuctionEngine(server) {");
-fs.writeFileSync('sockets/auctionEngine.js', code);
--- testRL.js
+++
@@ -1,9 +0,0 @@
-﻿const rateLimit = require('express-rate-limit');
-const limiter = rateLimit({
-    max: 1,
-    handler: (req, res, next, options) => {
-        console.log("OPTIONS:", options);
-    }
-});
-limiter({ ip: '1.2.3.4', headers: {} }, { setHeader: () => {}, status: () => ({ send: () => {}, json: () => {} }) }, () => {});
-limiter({ ip: '1.2.3.4', headers: {} }, { setHeader: () => {}, status: () => ({ send: () => {}, json: () => {} }) }, () => {});
